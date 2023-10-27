df06dde25ef1d3f2ee8a90f017b6baf175ab8616
Kafiirr <kafiirr.666@gmail.com>
2023-10-27 11:33:16 +0700

    added project

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import { ThirdwebProvider, ThirdwebSDKProvider } from "@thirdweb-dev/react";
+import '../styles/globals.css';
+
+const activeChain = {
+  chainId: 9398423,
+  rpc: ["https://froopyland.dymension.xyz/5/kafirdym_9398423-1/evmrpc"],
+  nativeCurrency: {
+    decimals: 18,
+    name: "KafirChain",
+    symbol: "KFR",
+  },
+  shortName: "KFR",
+  slug: "KafirChain",
+  testnet: true,
+  chain: "KafirChain",
+  name: "KafirChain Testnet",
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebSDKProvider activeChain={activeChain}>
+      <ThirdwebProvider
+        activeChain={activeChain}
+        clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      >
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+    </ThirdwebSDKProvider>
+  );
+}
+
+export default MyApp;
